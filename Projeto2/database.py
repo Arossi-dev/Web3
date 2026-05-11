b22555f963cb6a9a6e485fd59cde9771a7e6fb3d
Antonio Rossi <arossi.dev@gmail.com>
2026-05-10 21:50:17 -0300

    Add files via upload

---
+++ Projeto2/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+from models import Base
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    """Cria todas as tabelas definidas nos modelos."""
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    """Retorna uma sessão de banco (para uso na API)."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
